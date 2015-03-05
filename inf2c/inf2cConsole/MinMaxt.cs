47cf2746fb34418bcbed0cc150327e66173ba09f
HakobK <hakobk1@gmail.com>
2015-03-05 11:10:55 +0100

    fix

---
+++ inf2c/inf2cConsole/MinMaxt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using inf2c;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inf2cConsole
+{
+    class MinMaxt
+    {
+
+        public MinMaxt()
+        {
+
+            var testArray = new int[] { 1, 3, 5, 9, 29, 19, 2, 10, 203, 1, 3, 1 };
+
+            int min = MinMax.FindMin(testArray);
+            Console.WriteLine("Minimum number in array is: " + min);
+
+            int max = MinMax.FindMax(testArray);
+            Console.WriteLine("Maximum number in array is: " + max);
+        }
+
+    }
+}
--- inf2c/inf2cConsole/Program.cs
+++ inf2c/inf2cConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using inf2c;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace inf2cConsole
             SequentialSearchTest sequentialTest = new SequentialSearchTest();
 
             // dont close the console
+            MinMaxt MinMax = new MinMaxt();
             Console.ReadLine();
         }
     }
