8371abc0400c2808179806bf23e1c2001eb10581
Robert048 <Robertkarate@hotmail.com>
2015-03-05 11:10:39 +0100

    test

--- inf2c/inf2c/BinarySearch.cs
+++ inf2c/inf2c/BinarySearch.cs
@@ -9,7 +9,7 @@ namespace inf2c
     class BinarySearch
     {
         static void Main(string[] args)
-        {
+        {   
           Random random = new Random();
             int[] arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int value = binSearch(5, arr);
---
+++ inf2c/inf2cConsole/BinarySearchTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using inf2c;
+
+namespace inf2cConsole
+{
+    class BinarySearchTest
+    {
+        public BinarySearchTest()
+        {
+            var testArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            // search for 5 in testArray
+            int indexFound = BinarySearch.SeqSearch<int>(testArray, 29);
+            Console.WriteLine("[BinarySearch] 5 found at index: " + indexFound);
+
+        }
+
+    }
+}
