a8a6defb31470e262ba6794239cd3872023a7103
Robert048 <Robertkarate@hotmail.com>
2015-03-05 11:41:25 +0100

    BinarySearch

--- inf2c/inf2c/BinarySearch.cs
+++ inf2c/inf2c/BinarySearch.cs
@@ -8,24 +8,36 @@ namespace inf2c
 {
     public class BinarySearch
     {
-        public static int binSearch(int value, int[] arr)
+        /// <summary>
+        /// A binary search
+        /// </summary>
+        /// <typeparam name="T">generic type</typeparam>
+        /// <param name="value">searched value</param>
+        /// <param name="arr">arraylist with values</param>
+        /// <returns>The found position or -1 if nothing was found</returns>
+        public static int binSearch<T>(T value, T[] arr) where T : IComparable
         {
             int upperBound, lowerBound, mid;
             upperBound = arr.Length - 1;
             lowerBound = 0;
             while (lowerBound <= upperBound)
             {
+                //get the middle of the arraylist
                 mid = (upperBound + lowerBound) / 2;
-                if (arr[mid] == value)
+
+                //compare middle to the value and return middle if they are the same
+                if (arr[mid].CompareTo(value) == 0)
                 {
                     return mid;
                 }
                 else
                 {
-                    if (value < arr[mid])
+                    //if the value is smaller than the middle
+                    if (arr[mid].CompareTo(value) > 0)
                     {
                         upperBound = mid - 1;
                     }
+                    //the value is bigger than the middle
                     else
                     {
                         lowerBound = mid + 1;
