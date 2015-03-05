9b9196b6bf6f947ec6968d399f0eae2c2ffb31e1
Henrik Th <henrik.thien@student.stenden.com>
2015-03-05 12:14:31 +0100

    InsertionSort

---
+++ inf2c/inf2cConsole/InsertionSortTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using inf2c;
+
+namespace inf2cConsole
+{
+    class InsertionSortTest
+    {
+        public InsertionSortTest()
+        {
+            var array = new int[] { 4, 9, 1, 50, 38, 29, 2 };
+            var sortedArray = InsertionSort.InertionSort<int>(array);
+
+            var output = new StringBuilder();
+
+            foreach (var element in array)
+                output.Append(element + ", ");
+
+            Console.WriteLine("[InsertionSortTest] Unsorted array: " + output.ToString());
+            output.Clear();
+
+            foreach (var element in sortedArray)
+                output.Append(element + ", ");
+
+            Console.WriteLine("[InsertionSortTest] Sorted array: " + output.ToString());
+        }
+    }
+}
