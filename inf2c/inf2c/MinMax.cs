cdbc897e9a8378e05468ba1e80c218e1d8205205
HakobK <hakobk1@gmail.com>
2015-03-05 10:49:42 +0100

    minmax

--- inf2c/inf2c/MinMax.cs
+++ inf2c/inf2c/MinMax.cs
@@ -8,5 +8,28 @@ namespace inf2c
 {
     class MinMax
     {
+        static int FindMin(int[] arr)
+        {
+            int min = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] < min)
+                    min = arr[i];
+            }
+
+            return min;
+        }
+
+        static int FindMax(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > max)
+                    max = arr[i];
+            }
+
+            return max;
+        }
     }
 }
