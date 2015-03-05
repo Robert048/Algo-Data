886780ccf2dc38350ae10f7d4667745e29ecb5b2
Robert048 <Robertkarate@hotmail.com>
2015-03-05 10:56:28 +0100

    binarySearch test

---
+++ inf2c/inf2c/BinarySearch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inf2c
+{
+    class BinarySearch
+    {
+          Random random = new Random();
+            int[] arr = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int value = binSearch(5, arr);
+            Console.WriteLine(value);
+            Console.WriteLine("Done");
+            Console.Read();
+        }
+
+
+        static int binSearch(int value, int[] arr)
+        {
+            int upperBound, lowerBound, mid;
+            upperBound = arr.Length - 1;
+            lowerBound = 0;
+            while (lowerBound <= upperBound)
+            {
+                mid = (upperBound + lowerBound) / 2;
+                if (arr[mid] == value)
+                {
+                    return mid;
+                }
+                else
+                {
+                    if (value < arr[mid])
+                    {
+                        upperBound = mid - 1;
+                    }
+                    else
+                    {
+                        lowerBound = mid + 1;
+                    }
+                }
+            }
+            return -1;
+        }static void Main(string[] args)
+        {
+          
+       }
+}
