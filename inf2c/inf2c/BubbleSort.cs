a164ad65883519afa2cb2c726fb9f9b3d1ed821f
HakobK <hakobk1@gmail.com>
2015-03-05 12:07:15 +0100

    bubblesort

---
+++ inf2c/inf2c/BubbleSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inf2c
+{
+    public class BubbleSort
+    {
+
+        public static T[] BubbleSort<T>(T[] arr) where T : IComparable
+        {
+            int upper = arr.Length - 1;
+            T temp;
+
+            for (int outer = upper; outer >= 1; outer--) 
+            {
+                for (int inner = 0; inner <= outer - 1; inner++)
+                {
+                    if (arr[inner].CompareTo(arr[inner + 1]) > 0)
+                    {
+                        temp = arr[inner];
+                        arr[inner] = arr[inner + 1];
+                        arr[inner + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+
+        }
+    }
+}
---
+++ inf2c/inf2cConsole/BubbleSortTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using inf2c;
+
+namespace inf2cConsole
+{
+    class BubbleSortTest
+    {
+        public BubbleSortTest()
+        {
+
+            var testArray = new int[] { 1, 3, 5, 9, 29, 19, 2, 10, 203, 1, 3, 1 };
+            int[] number = BubbleSort.BubbleSort<int>(testArray);
+
+        }
+
+    }
+}
