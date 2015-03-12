ae978614956fd03a7e1d5588ed2033293174de41
kevinvording <kevin.mike.vording@hotmail.com>
2015-03-12 13:05:42 +0100

    Node, LinkedList

---
+++ inf2c/inf2c/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inf2c
+{
+    class Node
+    {
+        public Object Element;
+        public Node Link;
+        public Node()
+        {
+            Element = null;
+            Link = null;
+
+        }
+        public Node(Object theElement)
+        {
+            Element = theElement;
+            Link = null;
+        }
+    }
+}
