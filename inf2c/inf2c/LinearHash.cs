9dca5318df540ba1aadbcaee4f3285401441eb55
HenrikThien <henrik.thien@student.stenden.com>
2015-03-30 13:41:26 +0200

    LinearHash

---
+++ inf2c/inf2c/LinearHash.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inf2c
+{
+    public class LinearHash<T>
+    {
+        private int size;
+        private ArrayList[] data;
+
+        public LinearHash(int size)
+        {
+            this.size = size;
+            data = new ArrayList[size];
+        }
+
+        public void Output()
+        {
+            for (int i = 0; i < data.Count(); i++)
+            {
+                if (data[i] != null)
+                {
+                    Console.WriteLine(data[i].ToString());
+                }
+            }
+        }
+
+        public int HashFunction(int key)
+        {
+            return key % size;
+        }
+
+        public void Insert(T item)
+        {
+            
+        }
+    }
+}
