e9481ae44aea29b87f9414f4e227b1e231598e0f
HenrikThien <henrik.thien@student.stenden.com>
2015-03-30 13:20:49 +0200

    MinMax generics

--- inf2c/inf2c/MinMax.cs
+++ inf2c/inf2c/MinMax.cs
@@ -6,27 +6,42 @@ using System.Threading.Tasks;
 
 namespace inf2c
 {
-    public class MinMax
+    /// <summary>
+    /// Searching for a min and a max value in a given array
+    /// </summary>
+    /// <typeparam name="T">Any type of object which implements the IComparable interface</typeparam>
+    public class MinMax<T> where T : IComparable
     {
-
-        public static int FindMin(int[] arr)
+        /// <summary>
+        /// Find the min value in an array
+        /// </summary>
+        /// <param name="arr">The array to search the value in</param>
+        /// <returns>The found min value</returns>
+        public T FindMin(T[] arr)
         {
-            int min = arr[0];
+            T min = arr[0];
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                if (arr[i] < min)
+                // compare the current element with the min element
+                if (arr[i].CompareTo(min) < 0)
                     min = arr[i];
             }
 
             return min;
         }
 
-        public static int FindMax(int[] arr)
+        /// <summary>
+        /// Find the max value in an array
+        /// </summary>
+        /// <param name="arr">The array to search the value in</param>
+        /// <returns>The found max value</returns>
+        public T FindMax(T[] arr)
         {
-            int max = arr[0];
+            T max = arr[0];
             for (int i = 0; i < arr.Length - 1; i++)
             {
-                if (arr[i] > max)
+                // compare the current element with the max element
+                if (arr[i].CompareTo(max) > 0)
                     max = arr[i];
             }
 
