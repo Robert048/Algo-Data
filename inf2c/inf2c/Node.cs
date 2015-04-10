c740572a8e91f66b9096422f868c96b21034cb3f
kevinvording <kevin.mike.vording@hotmail.com>
2015-04-10 11:13:56 +0200

    Comments to Node Class

--- inf2c/inf2c/Node.cs
+++ inf2c/inf2c/Node.cs
@@ -6,21 +6,34 @@ using System.Threading.Tasks;
 
 namespace inf2c
 {
+
+    /// <summary>
+    /// Constructor for the Node Class.
+    /// </summary>
     public class Node
     {
         public Object Element;
         public Node Link;
+
+        /// <summary>
+        /// Setting the Element and Link to nothing when instantiated.
+        /// </summary>
         public Node()
         {
             Element = null;
             Link = null;
-
         }
+
+        /// <summary>
+        /// Assigns data to the Element member and sets the link to null.
+        /// </summary>
+        /// <param name="theElement">Element Object</param>
         public Node(Object theElement)
         {
             Element = theElement;
             Link = null;
         }
+
 
         public class InsertBeforeHeaderException : System.ApplicationException
         {
